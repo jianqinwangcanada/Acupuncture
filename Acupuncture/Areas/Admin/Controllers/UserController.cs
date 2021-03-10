fd211e0bf5fc22271d174356dc55c7aa83514335
jianqin wang <jianqin.wang01@stclairconnect.ca>
2021-03-10 10:39:56 -0500

    Complete Add User Edit User Delete User(softdelete)

--- Acupuncture/Areas/Admin/Controllers/UserController.cs
+++ Acupuncture/Areas/Admin/Controllers/UserController.cs
@@ -2,19 +2,65 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Acupuncture.CommonFunction.CookieFunction;
+using Acupuncture.CommonFunction.UserSvc;
+using Acupuncture.CommonFunction.WritebleAppSettingFunction;
+using Acupuncture.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Acupuncture.CommonFunction.AttributeFuction;
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Acupuncture.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(AuthenticationSchemes ="Admin")]
     public class UserController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly IUserSvc _userSvc;
+        private readonly ICookieSvc _cookieSvc;
+        private readonly IServiceProvider _provider;
+        private readonly DataProtectionKeys _dataProtectionKeys;
+        private readonly AppSettings _appSettings;
+        private  AdminBaseViewModel _adminBaseViewModel;
+        private readonly IWritebleSettingSvc<SiteWideSettings> _writebleSettingSvc;
+
+
+        public UserController(IUserSvc userSvc, ICookieSvc cookieSvc, IServiceProvider provider,
+            IOptions<DataProtectionKeys> dataProtectionKeys, IOptions<AppSettings> appSettings, IWritebleSettingSvc<SiteWideSettings> writableSiteWideSettings)
         {
-            return View();
+
+            _userSvc = userSvc;
+            _cookieSvc = cookieSvc;
+            _provider = provider;
+            _dataProtectionKeys = dataProtectionKeys.Value;
+            _appSettings = appSettings.Value;
+            _writebleSettingSvc = writableSiteWideSettings;
+        }
+
+
+        public  async Task<IActionResult> Index()
+        {
+            var protectorProvider = _provider.GetService<IDataProtectionProvider>();
+            var protector = protectorProvider.CreateProtector(_dataProtectionKeys.ApplicationUserKey);
+            var userProfile = await _userSvc.GetUserProfileByIdAsync(protector.Unprotect(_cookieSvc.Get("user_id")));
+            var addUserModel = new AddUserModel();
+            _adminBaseViewModel = new AdminBaseViewModel
+            {
+                Profile = userProfile,
+                AddUser = addUserModel,
+                AppSetting = _appSettings,
+                SiteWideSetting = _writebleSettingSvc.Value
+            };
+            return View("Index", _adminBaseViewModel);
+        }
+        [AjaxAccessOnly]
+        public IActionResult GetUsers()
+        {
+            return PartialView("_GetUsersLayout");
         }
     }
 }
--- Acupuncture/CommonFunction/WritebleAppSettingFunction/IWritebleSettingSvc.cs
+++ Acupuncture/CommonFunction/WritebleAppSettingFunction/IWritebleSettingSvc.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using Microsoft.Extensions.Options;
+
 namespace Acupuncture.CommonFunction.WritebleAppSettingFunction
 {
-    public interface IWritebleSettingSvc<out T>
+    public interface IWritebleSettingSvc<out T>: IOptionsSnapshot<T> where T : class, new()
     {
         bool Update(Action<T> applyChange);
     }
